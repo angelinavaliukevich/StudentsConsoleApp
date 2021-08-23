a15e0b439592a527ee91ee543d605f7772965586
ангелина валюкевич <angelinka.valyukevich@mail.ru>
2021-08-23 20:45:51 +0300

    init commit

---
+++ StudentsConsoleApp/StudentsConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
